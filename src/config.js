66bda477fe9bee0b172b51f7bd4d5afa13b89d3b
rmorales91 <rmorales@defontana.com>
2020-07-26 02:40:41 -0400

    Setting Up React App...

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-rey-app-files"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://lvnorj9yub.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_dPgmcU2MD",
+      APP_CLIENT_ID: "7jc8v5m4l93i8e51i66n708jk7",
+      IDENTITY_POOL_ID: "us-east-2:abd05a82-d56d-490d-8862-41b7187dd6a0"
+    }
+  };
